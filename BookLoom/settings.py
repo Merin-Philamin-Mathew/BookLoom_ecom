505ad9160a24be4481c6f662510a6e5adc95241d
Merin Mathew <merinphilaminmathew19@gmail.com>
2024-03-12 14:04:01 +0530

    git ingnre done

--- BookLoom/settings.py
+++ BookLoom/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$qcm=6$xw+eiypm+p-0dpo&!es6%=95s5(or0yqqrqp=2u3+t*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -106,9 +107,9 @@ WSGI_APPLICATION = 'BookLoom.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'BookLoom',
-        'USER': 'postgres',
-        'PASSWORD': '123',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -178,13 +179,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #mer configuring the new usermodel
 AUTH_USER_MODEL = 'adminapp.NewUser'
 
-RAZOR_PAY_KEY_ID = 'rzp_test_fGwLaAdAhjOjbm'
-KEY_SECRET = '7qO0FhI7y3SZGPpikcUEvVf1'
+RAZOR_PAY_KEY_ID = config('RAZOR_PAY_KEY_ID')
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'merinphilaminmathew19@gmail.com'
+KEY_SECRET =  config('KEY_SECRET') 
+EMAIL_BACKEND =  config('EMAIL_BACKEND') 
+EMAIL_HOST =  config('EMAIL_HOST') 
+EMAIL_USE_TLS =  config('EMAIL_USE_TLS') 
+EMAIL_PORT =  config('EMAIL_PORT') 
+EMAIL_HOST_USER =  config('EMAIL_HOST_USER') 
 
-EMAIL_HOST_PASSWORD = 'uevn atis igov cywb'
+EMAIL_HOST_PASSWORD =  config('EMAIL_HOST_PASSWORD') 
